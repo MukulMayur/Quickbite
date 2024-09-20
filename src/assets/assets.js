39ebe09a64af1ddd3448639e7711b459ecf8412d
Mukul <mukulmayur000@gmail.com>
2024-09-20 19:10:46 +0530

    Fix:Image issue

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,7 +1,7 @@
 import basket_icon from "./basket_icon.png";
 import logo from "./logo.png";
 import logo1 from "./logo1.png";
-import header_img from "./header_img.png";
+import headerimg from "./headerimg.png";
 import search_icon from "./search_icon.png";
 import menu_1 from "./menu_1.png";
 import menu_2 from "./menu_2.png";
@@ -65,7 +65,7 @@ export const assets = {
   logo,
   logo1,
   basket_icon,
-  header_img,
+  headerimg,
   search_icon,
   rating_starts,
   add_icon_green,
