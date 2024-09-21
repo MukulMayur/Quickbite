56592893251fe3d5f312786a8d81240023dcf920
Mukul <mukulmayur000@gmail.com>
2024-09-21 08:40:08 +0530

    Create:Style Component

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import StoreContextProvider from "./context/StoreContext.jsx";
+import "./style.css";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
