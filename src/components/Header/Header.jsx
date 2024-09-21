4349616ca4deb86fb9f31d3080a50a7b98bee151
Mukul <mukulmayur000@gmail.com>
2024-09-21 09:55:47 +0530

    ViewMenu Setup

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../style.css";
 
 const Header = () => {
+  const [menu, setMenu] = useState("home");
   return (
     <div className="header">
       <div className="header-contents">
         <p className="header-text">Order Your</p>
         <p className="header-text">Favourite</p>
         <p className="header-text">Food Here</p>
-        <button>View Menu</button>
+        <a href="#explore-menu" onClick={() => setMenu("menu")}>
+          <button>View Menu</button>
+        </a>
       </div>
     </div>
   );
